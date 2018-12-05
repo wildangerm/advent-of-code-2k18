2d851ef7ec133726dacf52dc79ebcdef9749930e
Martin Wildanger <wildanger.m@gmail.com>
2018-12-05 23:03:52 +0100

    Solved part2
    Refactored part1

--- src/advent_of_code_2k18/day5/Day5.java
+++ src/advent_of_code_2k18/day5/Day5.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import advent_of_code_2k18.BaseDay;
@@ -20,17 +24,59 @@ public class Day5 extends BaseDay {
 	@Override
 	public void part1() {
 
+		int shortestPolymer = react(inputList);
+
+		System.out.println("shortest: " + shortestPolymer);
+
+	}
+
+	private class ReactedComparator implements Comparator<Character> {
+
+		@Override
+		public int compare(Character c1, Character c2) {
+			List<Character> reducedList1 = new ArrayList<>(inputList);
+			reducedList1.removeAll(new ArrayList<>(Arrays.asList(c1, Character.toUpperCase(c1))));
+
+			List<Character> reducedList2 = new ArrayList<>(inputList);
+			reducedList2.removeAll(new ArrayList<>(Arrays.asList(c2, Character.toUpperCase(c2))));
+
+			return react(reducedList1).compareTo(react(reducedList2));
+		}
+
+	}
+
+	@Override
+	public void part2() {
+
+		Set<Character> charSet = new HashSet<>();
+
+		inputList.stream().map(Character::toLowerCase).forEach(ch -> {
+			charSet.add(ch);
+		});
+
+		Character c = charSet.stream().min(new ReactedComparator()).get();
+
+		List<Character> reducedList = new ArrayList<>(inputList);
+		reducedList.removeAll(new ArrayList<>(Arrays.asList(c, Character.toUpperCase(c))));
+
+		int shortestPolymer = react(reducedList);
+
+		System.out.println("shortest: " + shortestPolymer);
+	}
+
+	private Integer react(List<Character> inputList) {
+		List<Character> list = new ArrayList<>(inputList);
 		boolean finished = false;
 
 		while (!finished) {
 
-			int size = inputList.size();
+			int size = list.size();
 			Character previous = Character.MIN_VALUE;
 			List<Integer> toDelete = new ArrayList<>();
 
-			for (int i = 0; i < inputList.size(); i++) {
+			for (int i = 0; i < list.size(); i++) {
 
-				Character c = inputList.get(i);
+				Character c = list.get(i);
 				if (!c.equals(previous)
 						&& (c.equals(Character.toUpperCase(previous)) || c.equals(Character.toLowerCase(previous)))) {
 
@@ -44,22 +90,15 @@ public class Day5 extends BaseDay {
 			}
 
 			for (Integer integer : toDelete) {
-				inputList.remove(integer.intValue());
+				list.remove(integer.intValue());
 			}
 
-			if (inputList.size() == size) {
+			if (list.size() == size) {
 				finished = true;
 			}
 		}
 
-		System.out.println(inputList.size());
-
-	}
-
-	@Override
-	public void part2() {
-		// TODO Auto-generated method stub
-
+		return list.size();
 	}
 
 	private void readFile() {
