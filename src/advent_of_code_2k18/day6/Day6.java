a3097751487ca0036fbd88bf0d3e1c05c6442a8a
Martin Wildanger <wildanger.m@gmail.com>
2018-12-06 19:25:57 +0100

    Created grid

--- src/advent_of_code_2k18/day6/Day6.java
+++ src/advent_of_code_2k18/day6/Day6.java
@@ -13,12 +13,17 @@ import advent_of_code_2k18.BaseDay;
 
 public class Day6 extends BaseDay {
 
+	private List<Coordinate> inputList;
+	private int[][] grid;
+
 	public Day6() {
 		readFile();
-		System.out.println();
-	}
+		
+		int maxCoordX = inputList.stream().max((c1, c2) -> Integer.compare(c1.getX(), c2.getX())).get().getX();
+		int maxCoordY = inputList.stream().max((c1, c2) -> Integer.compare(c1.getY(), c2.getY())).get().getY();
 
-	private List<Coordinate> inputList;
+		grid = new int[maxCoordX][maxCoordY];
+	}
 
 	@Override
 	public void part1() {
@@ -50,11 +55,11 @@ public class Day6 extends BaseDay {
 
 		Pattern p = Pattern.compile(re1 + re2 + re3 + re4, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 		Matcher m = p.matcher(line);
-		
+
 		if (m.find()) {
 			int x = Integer.parseInt(m.group(1));
 			int y = Integer.parseInt(m.group(4));
-			
+
 			return new Coordinate(x, y);
 		}
 		return null;
