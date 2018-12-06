063de868f36e2e8eae491159f84db9673b9fd9a7
Martin Wildanger <wildanger.m@gmail.com>
2018-12-06 23:25:06 +0100

    Solved part1

--- src/advent_of_code_2k18/day6/Day6.java
+++ src/advent_of_code_2k18/day6/Day6.java
@@ -3,17 +3,24 @@ package advent_of_code_2k18.day6;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.function.Function;
 
 import advent_of_code_2k18.BaseDay;
 
 public class Day6 extends BaseDay {
 
-	private List<Coordinate> inputList;
+	private List<Coordinate> inputList = new ArrayList<>();
 	private int[][] grid;
 
 	public Day6() {
@@ -32,15 +39,88 @@ public class Day6 extends BaseDay {
 				grid[x][y] = getClosestCoordinateID(x, y);
 			}
 		}
-		for (int y = 0; y < grid[0].length; y++) {
-			for (int x = 0; x < grid.length; x++) {
-				System.out.print(grid[x][y] + "\t");
-				if (x == grid.length - 1) {
-					System.out.println();
-				}
+
+		printGrid();
+
+		List<Coordinate> candidates = excludeInfinite();
+		
+		List<Integer> candidateIDs = candidates.stream().map(Coordinate::getID).collect(Collectors.toList());
+
+		List<Integer> flatFilteredGrid = Arrays.stream(grid)
+												.flatMapToInt(x -> Arrays.stream(x))
+												.filter(x -> candidateIDs.contains(x))
+												.boxed()
+												.collect(Collectors.toList());
+
+		Long max = flatFilteredGrid.stream()
+									.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+									.entrySet()
+									.stream()
+									.max(Comparator.comparing(Entry::getValue))
+									.get()
+									.getValue();
+
+		System.out.println("max: " + max);
+
+	}
+
+	@Override
+	public void part2() {
+		// TODO Auto-generated method stub
+	
+	}
+
+	private List<Coordinate> excludeInfinite() {
+		List<Coordinate> candidates = new ArrayList<>();
+
+		Set<Integer> toExclude = getIDsToExclude();
+
+		inputList.forEach(coord -> {
+			if (!toExclude.contains(coord.getID())) {
+				candidates.add(coord);
+			}
+		});
+
+		return candidates;
+	}
+
+	private Set<Integer> getIDsToExclude() {
+		Set<Integer> toExclude = new HashSet<>();
+
+		// Walk the outer edges, and if we find an ID, it is to be excluded
+		// Top
+		for (int x = 0; x < grid.length; x++) {
+			int gridElement = grid[x][0];
+			if (gridElement != -1) {
+				toExclude.add(gridElement);
 			}
 		}
-		System.out.println();
+
+		// Bottom
+		for (int x = 0; x < grid.length; x++) {
+			int gridElement = grid[x][grid[0].length - 1];
+			if (gridElement != -1) {
+				toExclude.add(gridElement);
+			}
+		}
+
+		// Left
+		for (int y = 0; y < grid[0].length; y++) {
+			int gridElement = grid[0][y];
+			if (gridElement != -1) {
+				toExclude.add(gridElement);
+			}
+		}
+
+		// Right
+		for (int y = 0; y < grid[0].length; y++) {
+			int gridElement = grid[grid.length - 1][y];
+			if (gridElement != -1) {
+				toExclude.add(gridElement);
+			}
+		}
+
+		return toExclude;
 	}
 
 	private int getClosestCoordinateID(int x, int y) {
@@ -50,7 +130,7 @@ public class Day6 extends BaseDay {
 
 		for (Coordinate coordinate : inputList) {
 			if (coordinate.getX() == x && coordinate.getY() == y) {
-				return coordinate.getID() * 100;
+				return coordinate.getID();
 			}
 			int dist = getDistToCoordinate(coordinate, x, y);
 			if (dist < min) {
@@ -68,14 +148,20 @@ public class Day6 extends BaseDay {
 		return Math.abs(c.getX() - x) + Math.abs(c.getY() - y);
 	}
 
-	@Override
-	public void part2() {
-		// TODO Auto-generated method stub
-
+	private void printGrid() {
+		for (int y = 0; y < grid[0].length; y++) {
+			for (int x = 0; x < grid.length; x++) {
+				System.out.print(grid[x][y] + "\t");
+				if (x == grid.length - 1) {
+					System.out.println();
+				}
+			}
+		}
+		System.out.println();
 	}
 
 	private void readFile() {
-		String pathString = getPathStringToInput("testinput.txt");
+		String pathString = getPathStringToInput("input.txt");
 
 		try (Stream<String> stream = Files.lines(Paths.get(pathString))) {
 			inputList = stream.map(line -> createCoordinateFromRegex(line)).collect(Collectors.toList());
